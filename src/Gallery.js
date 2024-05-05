1644541929c10658c8cd80ab7cc0a7fa408b79c2
anudevSaraswat <mwanudev@gmail.com>
2024-05-05 17:13:48 +0530

    wip: added key prop in Profile component

--- src/Gallery.js
+++ src/Gallery.js
@@ -3,7 +3,7 @@ import data from "./data/users.json"
 
 export function Gallery() {
     let users = data.filter(user => user.numberOfAwards === 2)
-    let listItems = users.map(user => <Profile person={user} size={50}></Profile>) 
+    let listItems = users.map(user => <Profile key={user.id} person={user} size={50}></Profile>) 
     return (
         <div>
             {listItems}
